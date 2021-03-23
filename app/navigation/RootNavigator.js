0a29c152e1f4c67f0cdfc961137ee86296cb9e28
Massy Haddad <massy.haddad@lepisc.com>
2021-03-22 22:05:41 -0400

    Navigation bar

--- app/navigation/RootNavigator.js
+++ app/navigation/RootNavigator.js
@@ -1,10 +1,52 @@
 import * as React from "react";
+
+import { Text, View } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createSharedElementStackNavigator } from "react-navigation-shared-element";
+
 import HomeScreen from "../screens/HomeScreen";
 import DetailScreen from "../screens/DetailScreen";
 
 const Stack = createSharedElementStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function OptionsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Options!</Text>
+    </View>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            <View style={{}}>
+              <FontAwesome5 name="home" size={24} color="#CDCCCE" />
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 const options = {
   headerBackTitleVisible: false,
@@ -20,8 +62,11 @@ const options = {
 export default function RootNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator headerMode="none" initialRouteName="HomeScreen">
+      <Stack.Navigator headerMode="none" initialRouteName="Tabs">
+        <Stack.Screen name="Tabs" component={MyTabs} options={() => options} />
+
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+
         <Stack.Screen
           name="DetailScreen"
           component={DetailScreen}
